5e8c1dde910dc204f4d221a788d063f0e8f50995
Niloy-Sumon <niloyabidur@gmail.com>
2021-08-03 04:09:52 +0600

    card item add

--- src/App.js
+++ src/App.js
@@ -14,7 +14,10 @@ function App() {
           <Route exact path='/'>
             <Home/>
           </Route>
-          <Route exact path='/login'>
+          <Route path='/home'>
+            <Home/>
+          </Route>
+          <Route path='/login'>
             <Login/>
           </Route>
         </Switch>
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,18 +1,20 @@
 import React from 'react';
 
 const Card = (props) => {
-    const {title, taka} = props.card
-    return (
-        <div>
-            <div class="card" style={{width: '15rem'}}>
-  <div class="card-body">
-    <h5 class="card-title">{title}</h5>
-    <a href="/" class="btn btn-primary">Go somewhere</a>
-    <p class="card-text">{taka}</p>
-  </div>
-</div>
+  const { title, taka } = props.card
+  return (
+    <div>
+      <div>
+        <div class="card m-1" style={{ width: '15rem' }}>
+          <div class="card-body">
+            <h5 class="card-title">{title}</h5>
+            <button type="button" class="btn btn-primary">BUY NOW</button>
+            <p class="card-text">{taka}</p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Card;
